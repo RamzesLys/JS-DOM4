32b4c2b7a58ca16fd106601a4694a6de6197c740
RamzesLys <ramzes311982@gmail.com>
2022-12-07 12:58:32 +0200

    slider complete

--- scripts/script.js
+++ scripts/script.js
@@ -5,52 +5,58 @@ const [btnLeft, btnRight] = document.getElementsByClassName('biography__btn-arro
 
 let shift = 0;
 
+const width = -916;
+
 const sliderContentList = [
-{
-	content: `Molestie ultricies quam. Donec sem. 
+	{
+		content: `Molestie ultricies quam. Donec sem. 
 	Praesent pretium.
 	Maorbi quis nulla vehicula felsd laoreet.
 	Sed ullamcorper arcu ante sed ipsum tempus
 	tempor orcide. Nulla vierra ultrices magnal
 	Nam rutrum congue diam. Vestibulum non lorem.
 	Aliquam ullamcorper sem.`
-},
-{
-	content: `Lorem ipsum dolor sit amet consectetur 
+	},
+	{
+		content: `Lorem ipsum dolor sit amet consectetur 
 	adipisicing elit. Porro amet quisquam ducimus odit 
 	eum sapiente saepe. Facilis repellendus incidunt nihil.`
-},
-{
-	content: `Lorem ipsum dolor sit amet consectetur 
+	},
+	{
+		content: `Lorem ipsum dolor sit amet consectetur 
 	adipisicing elit. Porro amet quisquam ducimus odit 
 	eum sapiente saepe. orem ipsum dolor sit amet consectetur 
 	adipisicing elit. Facilis repellendus incidunt nihil.`
-},
-{
-	content: `Lorem ipsum dolor sit amet consectetur 
+	},
+	{
+		content: `Lorem ipsum dolor sit amet consectetur 
 	adipisicing elit. `
-}
+	}
 ];
 
 sliderContentList.forEach(item => {
-    const comment = document.createElement('li');
-    comment.textContent = item.content;
-    sliderList.append(comment);
-    comment.classList.add('biography__text')
+	const comment = document.createElement('li');
+	comment.textContent = item.content;
+	sliderList.append(comment);
+	comment.classList.add('biography__text')
 
 });
 
 btnLeft.addEventListener('click', (event) => {
-  shift = +shift + 916;
+	if (shift === 0) {
+		shift = width * (sliderContentList.length - 1);
+	} else {
+		shift = shift - width
+	}
 	sliderList.style.left = shift + 'px';
-
 });
 
 btnRight.addEventListener('click', (event) => {
-	shift = +shift - 916;
+	if (shift < width * (sliderContentList.length - 2)) {
+		shift = 0;
+	} else {
+		shift = shift + width
+	}
 	sliderList.style.left = shift + 'px';
-	console.log(shift);
-
 });
 
-console.log(sliderList, btnLeft, btnRight);
