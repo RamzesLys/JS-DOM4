aa81bfabc5cb8f3d787ab2594dc6b8a93583c292
RamzesLys <ramzes311982@gmail.com>
2022-12-07 10:13:14 +0200

    add script

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+console.log('Hi, Ramzes');
+
+const sliderContentList = [
+    {
+        content: `Molestie ultricies quam. Donec sem. Praesent pretium.
+        Maorbi quis nulla vehicula felsd laoreet.
+        Sed ullamcorper arcu ante sed ipsum tempus
+        tempor orcide. Nulla vierra ultrices magnal
+        Nam rutrum congue diam. Vestibulum non lorem.
+        Aliquam ullamcorper sem.`
+
+    }
+]
