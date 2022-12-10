5b31254a34b6465570e66d004b584589b8600985
RamzesLys <ramzes311982@gmail.com>
2022-12-10 14:57:44 +0200

    complete

--- scripts/myFirstSlide.js
+++ scripts/myFirstSlide.js
@@ -7,11 +7,53 @@ revSliderItem[2].classList.add('active');
 
 let index = 2;
 
+const cleaning = () => {
+	revSliderItem.forEach(item => {
+		item.classList.contains('active') && item.classList.remove('active');
+	})
+}
+
+revSliderItem.forEach((item, idx) => {
+	item.addEventListener('click', () => {
+		index = idx;
+		currentSlide(index);
+	})
+})
+
 const activeSlide = (item) => {
 	for (revSliderItem of revSliderList) {
 		revSliderItem.classList.remove('active');
 	}
-	slides[item].classList.add('active');
+	cleaning()
+	revSliderItem[item].classList.add('active');
+
 }
+
+const currentSlide = (idx) => {
+	activeSlide(idx)
+}
+
+const nextSlide = () => {
+	if (index === revSliderItem.length - 1) {
+		index = 0
+		currentSlide(index);
+	} else {
+		index ++;
+		currentSlide(index);
+	}
+}
+
+const prevSlide = () => {
+	if (index === 0) {
+		index = revSliderItem.length - 1;
+		currentSlide(index);;
+	} else {
+		index --;
+		currentSlide(index);
+	}
+}
+
+btnNext.addEventListener('click', nextSlide);
+btnPrev.addEventListener('click', prevSlide);
 
 console.log(revSliderItem);
