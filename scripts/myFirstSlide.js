e3d419b60a83a8de91ad9cceb9647689fba08e7f
RamzesLys <ramzes311982@gmail.com>
2022-12-10 14:07:17 +0200

    restart

--- scripts/myFirstSlide.js
+++ scripts/myFirstSlide.js
@@ -1,32 +1,17 @@
-console.log('My slide');
+//my slide
 
-const [reviewsBtnLeft, reviewsBtnRight] = document.getElementsByClassName('reviews__btn-arrow');
-const reviewsSlider = document.getElementsByClassName('.reviews__slider');
-const reviewsItem = document.getElementsByClassName('reviews__item');
-reviewsItem[2].classList.add('active');
-const reviewsItemList = Array.from(reviewsItem);
+const [btnPrev, btnNext] = document.getElementsByClassName('reviews__btn-arrow');
+const revSliderList = document.getAnimations('reviews__slider');
+const revSliderItem = document.querySelectorAll('.reviews__item');
+revSliderItem[2].classList.add('active');
 
-const cleanActive = () => {
-	reviewsItemList.forEach(item => {
-		item.classList.contains('active') && item.classList.remove('active');
-	})
+let index = 2;
+
+const activeSlide = (item) => {
+	for (revSliderItem of revSliderList) {
+		revSliderItem.classList.remove('active');
+	}
+	slides[item].classList.add('active');
 }
 
-reviewsItemList.forEach((item, index) => {
-	item.dataset.index = index
-})
-
-reviewsBtnLeft.addEventListener('click', (index) => {
-	if (reviewsItemList[2].classList.contains('active')) {
-		reviewsItemList[2].classList.remove('active');
-		reviewsItemList[1].classList.add('active')
-	} 
-
-})
-
-reviewsBtnRight.addEventListener('click', (index) => {
-	if (reviewsItemList[2].classList.contains('active')) {
-		reviewsItemList[2].classList.remove('active');
-		reviewsItemList[3].classList.add('active')
-	} 
-})
+console.log(revSliderItem);
